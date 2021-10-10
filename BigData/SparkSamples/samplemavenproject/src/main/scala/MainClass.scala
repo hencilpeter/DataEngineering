543166ed706655666728966c54ed6226c3a9b036
hencilpeter <38835386+hencilpeter@users.noreply.github.com>
2021-10-10 19:47:44 +0800

    spark simple hello world projects.

---
+++ BigData/SparkSamples/samplemavenproject/src/main/scala/MainClass.scala
@@ -0,0 +1,7 @@
+
+
+object MainClass {
+  def main(args:Array[String]){
+    println("MainClass Invoked")
+  }
+}
---
+++ BigData/SparkSamples/samplesparkproject/src/main/scala/MainClass.scala
@@ -0,0 +1,10 @@
+import org.apache.spark.sql.SparkSession
+
+object MainClass extends App {
+  println("Spark Mainclass Invoked")
+   val sc = SparkSession.builder().master("local[1]")
+          .appName("sparkHelloWorld")
+          .getOrCreate()
+           
+    println("Spark Context created!!!!")
+}
